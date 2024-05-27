9790903dc15cb4e15ff22c246e67cde0cfbb3397
ramyapulletikurthi <46881361+ramyapulletikurthi@users.noreply.github.com>
2024-05-27 21:20:42 +0530

    Update Crossbr.java
    Removed the extra space

--- src/test/java/sel/Crossbr.java
+++ src/test/java/sel/Crossbr.java
@@ -38,7 +38,6 @@ public class Crossbr {
 		driver.findElement(By.partialLinkText("Enter the Store")).click();
 		driver.findElement(By.partialLinkText("Sign In")).click();
 		driver.findElement(By.name("username")).sendKeys("admin");
-		
 		driver.findElement(By.name("password")).clear();
 		driver.findElement(By.name("password")).sendKeys("admin");
 		driver.findElement(By.name("signon")).click();
